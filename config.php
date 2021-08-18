2c731eaff8396ff017650123167578f020763161
Yuk24 <89156336+Yuk24@users.noreply.github.com>
2021-08-19 01:58:22 +0800

    qianniao.chp

--- config.php
+++ config.php
@@ -6,11 +6,13 @@
  * @version 2.0
  */
 $config = [
-	'bduss' => "", //�˴������к�̨�鿴Ȩ�ް����BDUSS
-	'kw' => '', //�ڰ�������д��Ҫ���������̨������
-	'showpic' => true, //booleanֵ���Ƿ���ͼƬ�������ķ�������Դ�����ɹر�
-	'hideopt' => true, //booleanֵ���Ƿ����ز����˰�ť�����鿪����������������˽��
-	'hidetext'=> false, // booleanֵ���Ƿ�չʾ�������ݣ�����������չʾ�������������
-	'newNav'=>false, // �����µĵ�����壬���Ⱪ¶�ӿ�
-	'pwd'=>'',// ��������̨��Ҫ��֤������ռ�Ϊ����Ҫ����,�����ö�̬����
+	'bduss' => "
+   BDUSS: WpBalduTVpOflRjTWVRUEc4cmMtbFB6QXpLd2JnQn41V1ZMOGRLZFFLazZ5VVJoRVFBQUFBJCQAAAAAAAAAAAEAAABvKqJSztLLxsj9wqW1zbX3AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADo8HWE6PB1hM", //此处输入有后台查看权限吧务的BDUSS
+	'kw' => '千鸟official', //在吧名处填写需要公开吧务后台的贴吧
+	'showpic' => true, //boolean值，是否开启图片（不消耗服务器资源），可关闭
+	'hideopt' => false, //boolean值，是否隐藏操作人按钮，建议开启（保护操作人隐私）
+	'hidetext'=> false, // boolean值，是否展示贴子内容，如果开启则会展示帖子里面的内容
+	'newNav'=>false, // 启用新的导航面板，避免暴露接口
+	'pwd'=>'',// 进入吧务后台需要验证口令，留空即为不需要口令,可设置动态口令
+	
 ];
